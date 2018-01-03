141f9ccef862f948f44d769fe38040f2a88aea51
Spuges <spuges.henrik@gmail.com>
2018-01-03 18:15:42 +0200

    Tweaks.. Inputs and such

--- Assets/Scripts/Gameplay/Entities/Invader.cs
+++ Assets/Scripts/Gameplay/Entities/Invader.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Invader : Entity
 {
+    public DamageSource CollisionDamageSource;
+
     protected override void Start()
     {
         base.Start();
         InvaderManager.RegisterNewInvader(this);
 
+        if(CollisionDamageSource == null)
+            CollisionDamageSource = GetComponent<DamageSource>();
     }
 
     private void OnDisable()
     {
         InvaderManager.RemoveInvader(this);
+    }
+
+    private void Update()
+    {
+        if(!InvaderManager.IsInsideBounds(new Vector2(transform.position.x, transform.position.y)))
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Defender dfndr = null;
+        if(dfndr = collision.GetComponent<Defender>())
+        {
+            if(CollisionDamageSource)
+            {
+                dfndr.DamageTaken(CollisionDamageSource.Damage);
+
+                if (CollisionDamageSource.OnDamageDone != null)
+                    CollisionDamageSource.OnDamageDone.Invoke();
+            }
+        }
     }
 }
